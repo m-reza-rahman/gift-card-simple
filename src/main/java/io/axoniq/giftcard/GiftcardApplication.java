bfcf5b3f39d4b0c8ae7d906bd43f07a0217e1b69
Reza Rahman <reza_rahman@lycos.com>
2018-06-18 22:17:00 -0400

    Minor refactor.

--- src/main/java/io/axoniq/giftcard/GiftcardApplication.java
+++ src/main/java/io/axoniq/giftcard/GiftcardApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GiftcardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GiftcardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GiftcardApplication.class, args);
+    }
 
 }
--- src/main/java/io/axoniq/giftcard/command/GiftCard.java
+++ src/main/java/io/axoniq/giftcard/command/GiftCard.java
@@ -14,26 +14,26 @@ import static org.axonframework.commandhandling.model.AggregateLifecycle.apply;
 @Aggregate
 public class GiftCard {
 
-    private final static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @AggregateIdentifier
     private String id;
     private int remainingValue;
 
     public GiftCard() {
-        log.info("empty constructor invoked");
+        logger.info("empty constructor invoked");
     }
 
     @CommandHandler
     public GiftCard(IssueCmd cmd) {
-        log.info("handling {}", cmd);
+        logger.info("handling {}", cmd);
         if(cmd.getAmount() <= 0) throw new IllegalArgumentException("amount <= 0");
         apply(new IssuedEvt(cmd.getId(), cmd.getAmount()));
     }
 
     @CommandHandler
     public void handle(RedeemCmd cmd) {
-        log.info("handling {}", cmd);
+        logger.info("handling {}", cmd);
         if(cmd.getAmount() <= 0) throw new IllegalArgumentException("amount <= 0");
         if(cmd.getAmount() > remainingValue) throw new IllegalStateException("amount > remaining value");
         apply(new RedeemedEvt(id, cmd.getAmount()));
@@ -41,17 +41,17 @@ public class GiftCard {
 
     @EventSourcingHandler
     public void on(IssuedEvt evt) {
-        log.info("applying {}", evt);
+        logger.info("applying {}", evt);
         id = evt.getId();
         remainingValue = evt.getAmount();
-        log.info("new remaining value: {}", remainingValue);
+        logger.info("new remaining value: {}", remainingValue);
     }
 
     @EventSourcingHandler
     public void on(RedeemedEvt evt) {
-        log.info("applying {}", evt);
+        logger.info("applying {}", evt);
         remainingValue -= evt.getAmount();
-        log.info("new remaining value: {}", remainingValue);
+        logger.info("new remaining value: {}", remainingValue);
     }
 
 }
