ea52b94d3580e0daa35bfa11dc0ddea1aff7e76d
Reza Rahman <reza_rahman@lycos.com>
2018-06-26 18:36:40 -0400

    Streamlining code.

--- src/main/java/io/axoniq/giftcard/infrastructure/AxonConfiguration.java
+++ src/main/java/io/axoniq/giftcard/infrastructure/AxonConfiguration.java
@@ -1,14 +1,11 @@
 package io.axoniq.giftcard.infrastructure;
 
-import io.axoniq.giftcard.query.CardSummaryProjection;
-import org.axonframework.config.EventHandlingConfiguration;
 import org.axonframework.eventhandling.EventBus;
 import org.axonframework.eventhandling.SimpleEventBus;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.serialization.Serializer;
 import org.axonframework.serialization.json.JacksonSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,12 +13,6 @@ import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AxonConfiguration {
-
-    /* Using tracking processors for our read model, which will store their tokens. */
-    @Autowired
-    public void configure(EventHandlingConfiguration configuration) {
-        configuration.registerTrackingProcessor(CardSummaryProjection.class.getPackage().getName());
-    }
 
     /* A persistent event bus (event store) for event sourcing in our command model. */
     @Bean
